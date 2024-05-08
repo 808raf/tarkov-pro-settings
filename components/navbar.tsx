c3b5a18fe311f48b97bb96d5836707e5cb52507f
808raf <102700868+808raf@users.noreply.github.com>
2024-05-07 19:29:16 -1000

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,11 +2,11 @@
 
 const Navbar = () => {
   return (
-    <ul className="flex justify-around border-b-2 h-14 items-center">
+    <ul className="flex justify-between border-b-2 h-14 items-center pl-6 pr-6">
       <div className="flex gap-6">
         <li>TarkovProSettings</li>
 
-        <div className="flex gap-6">
+        <div className="flex gap-6 pl-6">
           <li>Players</li>
           <li>Blog</li>
         </div>
