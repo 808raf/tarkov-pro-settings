44bc66184a9bf54175c9136b8aeb5e6e330a190f
808raf <102700868+808raf@users.noreply.github.com>
2024-05-07 19:40:44 -1000

    Update background color

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,18 @@
 "use client";
 
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/public/logo.png";
+
 const Navbar = () => {
   return (
-    <ul className="flex justify-between border-b-2 h-14 items-center pl-6 pr-6">
+    <ul className="flex justify-between border-b-2 h-14 items-center pl-6 pr-6 font-semibold">
       <div className="flex gap-6">
-        <li>TarkovProSettings</li>
+        <li>
+          <Link href={"/"}>
+            <Image src={logo} alt="logo" height={24} />
+          </Link>
+        </li>
 
         <div className="flex gap-6 pl-6">
           <li>Players</li>
