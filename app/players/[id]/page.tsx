593880c1f06a737532e763f6c6deed550b2bacdc
808raf <102700868+808raf@users.noreply.github.com>
2024-05-18 10:39:29 -1000

    Fix navbar links

--- app/players/[id]/page.tsx
+++ app/players/[id]/page.tsx
@@ -1,4 +1,5 @@
 import PlayerPage from "@/components/player/player-page";
+import { getPlayers } from "@/lib/getPlayers";
 
 const Page = ({ params }: { params: { id: string } }) => {
   return (
@@ -7,4 +8,13 @@ const Page = ({ params }: { params: { id: string } }) => {
     </main>
   );
 };
+
 export default Page;
+
+export async function generateStaticParams() {
+  const players = await getPlayers();
+
+  return players.map((player) => ({
+    id: player.twitch_username.toLowerCase(),
+  }));
+}
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,64 +6,51 @@ import { DarkToggle } from "./dark-toggle";
 
 const Navbar = () => {
   return (
-    <ul className="flex justify-between shadow-sm h-14 items-center pl-6 pr-6 font-bold text-sm bg-white dark:bg-slate-950">
-      <div className="flex shrink-0 items-center">
-        <li className="hidden sm:block">
-          <Link className="hover:bg-transparent" href={"/"}>
+    <nav className="shadow-sm h-14 bg-white dark:bg-slate-950">
+      <ul className="flex justify-between items-center h-full pl-6 pr-6 font-bold text-sm">
+        <li className="flex items-center gap-3">
+          <Link className="hidden sm:block hover:bg-transparent" href="/">
             <span className="text-2xl p-1 bg-gradient-to-t from-sky-900 to-sky-600 bg-clip-text text-transparent">
               TarkovProSettings
             </span>
           </Link>
-        </li>
-
-        <div className="flex pl-6">
-          <li>
-            <Link
-              className="hover:bg-sky-500 hover:text-white p-3 rounded-sm block"
-              href={"/players"}
-            >
-              Players
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="hover:bg-sky-500 hover:text-white p-3 rounded-sm block"
-              href={"/blog"}
-            >
-              Blog
-            </Link>
-          </li>
-        </div>
-      </div>
-
-      <div className="flex space-x-3">
-        <li>
           <Link
-            href={"/login"}
+            className="hover:bg-sky-500 hover:text-white p-3 rounded-sm block"
+            href="/players"
+          >
+            Players
+          </Link>
+          <Link
+            className="hover:bg-sky-500 hover:text-white p-3 rounded-sm block"
+            href="/blog"
+          >
+            Blog
+          </Link>
+        </li>
+        <li className="flex items-center space-x-3">
+          <Link
+            href="/login"
             className="hover:bg-transparent hover:text-black p-0"
           >
             <Button
               className="hover:bg-sky-500 hover:text-white font-bold text-xs"
-              variant={"outline"}
+              variant="outline"
             >
               Log In
             </Button>
           </Link>
-        </li>
-        <li>
-          <Link href={"/signup"} className="hover:bg-transparent p-0">
+          <Link href="/signup" className="hover:bg-transparent p-0">
             <Button
-              variant={"outline"}
+              variant="outline"
               className="hover:bg-sky-500 hover:text-white font-bold text-xs"
-              // className="bg-sky-700 hover:bg-sky-500 dark:hover:bg-sky-500 shadow-md dark:text-white dark:bg-slate-900"
             >
               Sign Up
             </Button>
           </Link>
+          <DarkToggle />
         </li>
-        <DarkToggle />
-      </div>
-    </ul>
+      </ul>
+    </nav>
   );
 };
 
