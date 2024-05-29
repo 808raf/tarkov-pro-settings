dc2d18709225bb9b822055da0b69d338a4d6a576
808raf <102700868+808raf@users.noreply.github.com>
2024-05-29 11:21:58 -1000

    Add sort by twitch_username to card-grid
    All cards are now sorted alphabetically in asc order.

--- components/player/card-grid.tsx
+++ components/player/card-grid.tsx
@@ -11,7 +11,17 @@ interface CardGridProps {
   players: Player[];
 }
 
+const sortPlayers = (playerObj: Player[]) => {
+  const players = [...playerObj];
+  let sortedPlayers = players.sort((a, b) =>
+    a.twitch_username.localeCompare(b.twitch_username)
+  );
+  return sortedPlayers;
+};
+
 const CardGrid: React.FC<CardGridProps> = ({ players }) => {
+  let sortedPlayers = sortPlayers(players);
+
   return (
     <section className="container mt-10 max-w-7xl mb-14">
       <h1 className="text-4xl font-bold mb-14 bg-gradient-to-b from-sky-400 to-sky-900 bg-clip-text text-transparent">
@@ -21,7 +31,7 @@ const CardGrid: React.FC<CardGridProps> = ({ players }) => {
         <PlayerSearch />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mb-14 gap-4">
-        {players.map((player) => (
+        {sortedPlayers.map((player) => (
           <PlayerCard
             key={player.twitch_username}
             username={player.twitch_username}
